f06bc4a1634fecccfd5057dccc96f8cfd7ece01f
Student <student@narxoz.kz>
2026-03-13 20:12:32 +0500

    // добавлены конкретные листья HeroUnit и EnemyUnit

--- src/com/narxoz/rpg/composite/EnemyUnit.java
+++ src/com/narxoz/rpg/composite/EnemyUnit.java
@@ -1,5 +1,6 @@
 package com.narxoz.rpg.composite;
 
+// добавлены конкретные листья HeroUnit и EnemyUnit
 public class EnemyUnit extends UnitLeaf {
     public EnemyUnit(String name, int health, int attackPower) {
         super(name, health, attackPower);

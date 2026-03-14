9d99b90b9dad1e73cb543f3d2edab5eb6f7823d5
Student <student@narxoz.kz>
2026-03-14 08:06:00 +0500

    добавлен интерфейс EffectImplementor

--- src/com/narxoz/rpg/bridge/EffectImplementor.java
+++ src/com/narxoz/rpg/bridge/EffectImplementor.java
@@ -1,5 +1,6 @@
 package com.narxoz.rpg.bridge;
 
+// добавила этот интерфейс и внесла логику расчёта урона отдельно от скиллов
 public interface EffectImplementor {
     int computeDamage(int basePower);
     String getEffectName();
